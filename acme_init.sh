12146a096d84cd7ee298b9735108ce0906d60820
IvonWei <madwind.cn@gmail.com>
2021-11-30 00:38:08 +0800

    Update acme_init.sh

--- acme_init.sh
+++ acme_init.sh
@@ -1,1 +1,11 @@
-
+set -ex
+if [ -n "${EMAIL}" -a -n "${DOMAIN}" ]; then
+  for domain in ${DOMAIN}
+    do
+      /root/.acme.sh/acme.sh --register-account -m ${EMAIL} --issue -d ${domain} --standalone
+      /root/.acme.sh/acme.sh --install-cert -d ${domain} \
+      --key-file       /etc/nginx/ssl/${domain}/key.pem  \
+      --fullchain-file /etc/nginx/ssl/${domain}/cert.pem \
+      --reloadcmd     "service nginx force-reload"
+    done
+fi

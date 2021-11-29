1a692da588295f7f0b7fd547287ecb3d66d75881
IvonWei <madwind.cn@gmail.com>
2021-11-30 00:50:05 +0800

    Update acme_init.sh

--- acme_init.sh
+++ acme_init.sh
@@ -3,6 +3,9 @@ if [ -n "${EMAIL}" -a -n "${DOMAIN}" ]; then
   for domain in ${DOMAIN}
     do
       /root/.acme.sh/acme.sh --register-account -m ${EMAIL} --issue -d ${domain} --standalone
+      if [ ! -d "/etc/nginx/ssl/${domain}" ]; then
+          mkdir -p /etc/nginx/ssl/${domain}
+      fi
       /root/.acme.sh/acme.sh --install-cert -d ${domain} \
       --key-file       /etc/nginx/ssl/${domain}/key.pem  \
       --fullchain-file /etc/nginx/ssl/${domain}/cert.pem \

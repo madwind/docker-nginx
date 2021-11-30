77a077b8bf3441f13817763db6fdbf3fb497bc44
IvonWei <madwind.cn@gmail.com>
2021-11-30 23:11:58 +0800

    Update acme_init.sh

--- acme_init.sh
+++ acme_init.sh
@@ -1,13 +1,14 @@
 if [ -n "${EMAIL}" -a -n "${DOMAIN}" ]; then
   for domain in ${DOMAIN}
     do
-      /root/.acme.sh/acme.sh --register-account -m ${EMAIL} --issue -d ${domain} --keylength ec-256 --standalone --config-home /etc/acme
+      /nginx/.acme.sh/acme.sh --register-account -m ${EMAIL} --issue -d ${domain} --keylength ec-256 --standalone --config-home /etc/acme
       if [ ! -d "/etc/nginx/ssl/${domain}" ]; then
           mkdir -p /etc/nginx/ssl/${domain}
       fi
-      /root/.acme.sh/acme.sh --install-cert -d ${domain} --ecc --config-home /etc/acme \
+      /nginx/.acme.sh/acme.sh --install-cert -d ${domain} --ecc --config-home /etc/acme \
       --key-file       /etc/nginx/ssl/${domain}/key.pem  \
       --fullchain-file /etc/nginx/ssl/${domain}/cert.pem \
       --reloadcmd     "netstat -anput | grep nginx && nginx -s reload"
     done
 fi
+nginx -g "deamon off;"

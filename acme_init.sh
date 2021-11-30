9b98104b68eb054fbac82f4ee0c6f0573a565ce2
IvonWei <madwind.cn@gmail.com>
2021-11-30 19:31:06 +0800

    Update acme_init.sh

--- acme_init.sh
+++ acme_init.sh
@@ -1,11 +1,11 @@
 if [ -n "${EMAIL}" -a -n "${DOMAIN}" ]; then
   for domain in ${DOMAIN}
     do
-      /root/.acme.sh/acme.sh --register-account -m ${EMAIL} --issue -d ${domain} --server letsencrypt --keylength ec-256 --standalone --config-home /etc/acme
+      /root/.acme.sh/acme.sh --register-account -m ${EMAIL} --issue -d ${domain} --keylength ec-256 --standalone --config-home /etc/acme
       if [ ! -d "/etc/nginx/ssl/${domain}" ]; then
           mkdir -p /etc/nginx/ssl/${domain}
       fi
-      /root/.acme.sh/acme.sh --install-cert -d ${domain} --config-home /etc/acme \
+      /root/.acme.sh/acme.sh --install-cert -d ${domain} --ecc --config-home /etc/acme \
       --key-file       /etc/nginx/ssl/${domain}/key.pem  \
       --fullchain-file /etc/nginx/ssl/${domain}/cert.pem \
       --reloadcmd     "netstat -anput | grep nginx && nginx -s reload"

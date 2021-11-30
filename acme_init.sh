d7c6801325667da59a021f8da032126347a6d592
IvonWei <madwind.cn@gmail.com>
2021-12-01 01:57:53 +0800

    Update acme_init.sh

--- acme_init.sh
+++ acme_init.sh
@@ -1,14 +1,19 @@
 if [ -n "${EMAIL}" -a -n "${DOMAIN}" ]; then
   for domain in ${DOMAIN}
     do
-      /nginx/.acme.sh/acme.sh --register-account -m ${EMAIL} --issue -d ${domain} --keylength ec-256 --standalone --config-home /etc/acme
       if [ ! -d "/etc/nginx/ssl/${domain}" ]; then
           mkdir -p /etc/nginx/ssl/${domain}
       fi
-      /nginx/.acme.sh/acme.sh --install-cert -d ${domain} --ecc --config-home /etc/acme \
-      --key-file       /etc/nginx/ssl/${domain}/key.pem  \
-      --fullchain-file /etc/nginx/ssl/${domain}/cert.pem \
-      --reloadcmd     "netstat -anput | grep nginx && nginx -s reload"
+      /nginx/.acme.sh/acme.sh --register-account -m ${EMAIL}
+                              --issue -d ${domain}
+                              --keylength ec-256
+                              --standalone
+                              --config-home /etc/acme
+      /root/.acme.sh/acme.sh --install-cert -d ${domain}
+                             --ecc
+                             --key-file /etc/nginx/ssl/${domain}/cert.pem \
+                             --fullchain-file /etc/nginx/ssl/${domain}/cert.key \
+                             --reloadcmd "netstat -anput | grep nginx && nginx -s reload"
+                             --config-home /etc/acme \
     done
 fi
-nginx -g "daemon off;"

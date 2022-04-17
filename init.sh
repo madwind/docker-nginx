71268638b688bfb89cfd7606583f20f003a71b3f
IvonWei <madwind.cn@gmail.com>
2022-04-17 13:31:25 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,31 +3,33 @@ if [ "$GEOIPUPDATE_ACCOUNT_ID" ] && [ "$GEOIPUPDATE_LICENSE_KEY" ] && [ "$GEOIPU
   if [ "$GEOIPUPDATE_DB_DIR" ]; then
     database_dir=$GEOIPUPDATE_DB_DIR
   fi
-  if [ -z "$(ls -A $database_dir)" ]; then
-       sh /geoipupdate.sh
+  if [ -z "$(ls -A "$database_dir")" ]; then
+    sh /geoipupdate.sh
   fi
 fi
 
-if [ -n "${EAB_KID}" -a -n "${EAB_HMAC_KEY}" -a -n "${DOMAIN}" ]; then
-  for domain in ${DOMAIN}
-    do
-      if [ ! -d "/etc/nginx/ssl/${domain}" ]; then
-          mkdir -p /etc/nginx/ssl/${domain}
-      fi
-      /root/.acme.sh/acme.sh --register-account \
-                             --eab-kid ${EAB_KID} \
-                             --eab-hmac-key ${EAB_HMAC_KEY} \
-                             --issue -d ${domain} \
-                             --keylength ec-256 \
-                             --standalone
-      /root/.acme.sh/acme.sh --install-cert -d ${domain} \
-                             --ecc \
-                             --key-file /etc/nginx/ssl/${domain}/cert.key \
-                             --fullchain-file /etc/nginx/ssl/${domain}/cert.pem \
-                             --reloadcmd "netstat -anput | grep nginx && nginx -s reload"
-    done
+if [ -n "${EAB_KID}" ] && [ -n "${EAB_HMAC_KEY}" ] && [ -n "${DOMAIN}" ]; then
+  /root/.acme.sh/acme.sh \
+    --register-account \
+    --eab-kid "${EAB_KID}" \
+    --eab-hmac-key "${EAB_HMAC_KEY}"
+  for domain in ${DOMAIN}; do
+    if [ ! -d "/etc/nginx/ssl/${domain}" ]; then
+      mkdir -p /etc/nginx/ssl/"${domain}"
+    fi
+    /root/.acme.sh/acme.sh \
+      --issue -d "${domain}" \
+      --keylength ec-256 \
+      --standalone
+    /root/.acme.sh/acme.sh \
+      --install-cert -d "${domain}" \
+      --ecc \
+      --key-file /etc/nginx/ssl/"${domain}"/cert.key \
+      --fullchain-file /etc/nginx/ssl/"${domain}"/cert.pem \
+      --reloadcmd "netstat -anput | grep nginx && nginx -s reload"
+  done
 fi
 
 if [ ! -d "/usr/local/nginx/proxy_cache" ]; then
-    mkdir -p /usr/local/nginx/proxy_cache
+  mkdir -p /usr/local/nginx/proxy_cache
 fi

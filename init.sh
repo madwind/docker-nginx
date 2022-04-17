12c2e662ef63d5121c776c699548c1a0e960a319
IvonWei <madwind.cn@gmail.com>
2022-04-17 11:49:17 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -15,18 +15,16 @@ if [ -n "${EAB_KID}" -a -n "${EAB_HMAC_KEY}" -a -n "${DOMAIN}" ]; then
           mkdir -p /etc/nginx/ssl/${domain}
       fi
       /root/.acme.sh/acme.sh --register-account \
-                             --eab-kid ${EAB_KID} \ 
+                             --eab-kid ${EAB_KID} \
                              --eab-hmac-key ${EAB_HMAC_KEY} \
                              --issue -d ${domain} \
                              --keylength ec-256 \
                              --standalone \
-                             --config-home /etc/acme
       /root/.acme.sh/acme.sh --install-cert -d ${domain} \
                              --ecc \
                              --key-file /etc/nginx/ssl/${domain}/cert.key \
                              --fullchain-file /etc/nginx/ssl/${domain}/cert.pem \
                              --reloadcmd "netstat -anput | grep nginx && nginx -s reload" \
-                             --config-home /etc/acme
     done
 fi
 

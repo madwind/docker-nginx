31bb43493837b47f446ce4cbd42f0545ab0cbe07
IvonWei <madwind.cn@gmail.com>
2025-06-19 10:27:40 +0800

    Update 50-envsubst-on-node.sh

--- 50-envsubst-on-node.sh
+++ 50-envsubst-on-node.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 if [ -f /etc/nginx/nginx.conf.template ]; then
-  envsubst '$NODE' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+  envsubst '$NODE_IP' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 fi
